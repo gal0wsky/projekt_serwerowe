7d0f5ac7d372c05f5ec219892f2b299e91c41c09
gal0wsky <maciej.gawlowski@zse.krakow.pl>
2021-11-19 16:21:43 +0100

    Dodanie plików zmodyfikowanych na lekcji --> 19.11.2021

--- php/towary.php
+++ php/towary.php
@@ -2,16 +2,16 @@
 
 function towary() {
     $towary = array(
-        0 => array("Nazwa" => "Towar 1", "Img" => "../img/Towar1.jpg", "Cena" => 605.00),
-        1 => array("Nazwa" => "Towar 2", "Img" => "../img/Towar2.jpg", "Cena" => 105.00),
-        2 => array("Nazwa" => "Towar 3", "Img" => "../img/Towar3.jpg", "Cena" => 300.00),
-        3 => array("Nazwa" => "Towar 4", "Img" => "../img/Towar4.jpg", "Cena" => 17.95),
-        4 => array("Nazwa" => "Towar 5", "Img" => "../img/Towar5.jpg", "Cena" => 212.99),
-        5 => array("Nazwa" => "Towar 6", "Img" => "../img/Towar6.jpg", "Cena" => 20.30),
-        6 => array("Nazwa" => "Towar 7", "Img" => "../img/Towar7.jpg", "Cena" => 69.00),
-        7 => array("Nazwa" => "Towar 8", "Img" => "../img/Towar8.jpg", "Cena" => 420.00),
-        8 => array("Nazwa" => "Towar 9", "Img" => "../img/Towar9.jpg", "Cena" => 190.98),
-        9 => array("Nazwa" => "Towar 10", "Img" => "../img/Towar10.jpg", "Cena" => 55.55),
+        0 => array("Nazwa" => "Towar 1", "Img" => "../img/Towar1.png", "Cena" => 605.00),
+        1 => array("Nazwa" => "Towar 2", "Img" => "../img/Towar2.png", "Cena" => 105.00),
+        2 => array("Nazwa" => "Towar 3", "Img" => "../img/Towar3.png", "Cena" => 300.00),
+        3 => array("Nazwa" => "Towar 4", "Img" => "../img/Towar4.png", "Cena" => 17.95),
+        4 => array("Nazwa" => "Towar 5", "Img" => "../img/Towar5.png", "Cena" => 212.99),
+        5 => array("Nazwa" => "Towar 6", "Img" => "../img/Towar6.png", "Cena" => 20.30),
+        6 => array("Nazwa" => "Towar 7", "Img" => "../img/Towar7.png", "Cena" => 69.00),
+        7 => array("Nazwa" => "Towar 8", "Img" => "../img/Towar8.png", "Cena" => 420.00),
+        8 => array("Nazwa" => "Towar 9", "Img" => "../img/Towar9.png", "Cena" => 190.98),
+        9 => array("Nazwa" => "Towar 10", "Img" => "../img/Towar10.png", "Cena" => 55.55),
     );
 
     $promocje = array(
